77cc77c560ec589906701a82783727e2b59534b9
yanglei <lei.yang@17zuoye.com>
2017-06-19 16:13:20 +0800

    update simhashIndex

--- cluster.go
+++ cluster.go
@@ -16,7 +16,8 @@ func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU() - 1)
 	beego.Run()
 	*/
-	ss := []string{"放大是否打算"}
+	ss := []string{"放大是否打算","发生的旅客合法的顺口溜合法的索科洛夫哈萨克了会发生", "11223344", "aabbcc",
+		"AABBCC", "AaBbcC"}
 	res := []simhash.Simhash{}
 	for _, s := range ss {
 		sim := simhash.Simhash{}
@@ -26,9 +27,16 @@ func main() {
 	for _, r := range res {
 		fmt.Println(r.Value())
 	}
-	n0 := simhash.IndexNode{}
-	n0.Init(res[0], "0")
-	test := []simhash.IndexNode{n0}
+	test := []simhash.IndexNode{}
 	s := simhash.SimhashIndex{}
+	for idx := range res {
+		tpNode := simhash.IndexNode{res[idx], fmt.Sprintf("%d", idx)}
+		test = append(test, tpNode)
+	}
 	s.Init(test)
+	ts := simhash.Simhash{}
+	ts.Init("aabbcc")
+	fmt.Println(ts.Value())
+	ans := s.GetNearDups(ts)
+	fmt.Println(ans)
 }
